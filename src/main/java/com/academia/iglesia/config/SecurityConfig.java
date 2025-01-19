145336f095a281ea254d2e0da6724b8c7b92d78c
lerghin <lerghincardenas@gmail.com>
2025-01-18 21:08:04 -0400

    test2

--- src/main/java/com/academia/iglesia/config/SecurityConfig.java
+++ src/main/java/com/academia/iglesia/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                                 .requestMatchers("/auth/**").permitAll()
                                 .requestMatchers("/admin/**").permitAll()
                                 .requestMatchers("/user/**").permitAll()
-                                .anyRequest().authenticated()
+                                .anyRequest().permitAll()
                 )
                 .sessionManagement(sessionManager ->
                         sessionManager
